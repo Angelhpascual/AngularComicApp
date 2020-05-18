6e13f9f25e81f204139f7342c55750295cedbf14
Angelhpascual <angelhpascual@gmail.com>
2020-05-18 21:56:51 +0200

    Search Properties Added

---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+})
+export class BuscadorComponent implements OnInit {
+
+  constructor(private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(params =>{
+      console.log(params['termino']);
+
+    })
+  }
+
+}
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -74,4 +74,21 @@ export class HeroesService {
   getHeroe( idx:string ){
     return this.heroes[idx];
   }
+  bucarHeroes(termino){
+
+    let heroesArr:Heroe[] = [];
+    termino = termino.toLowerCase();
+
+    for(let heroe of this.heroes){
+
+      let nombre = heroe.nombre.toLowerCase();
+
+      if (nombre.indexOf( termino ) >=0 ){
+        heroesArr.push ( heroe )
+      }
+
+
+    }
+    return heroesArr;
+  }
 }
